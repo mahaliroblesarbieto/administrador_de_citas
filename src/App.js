f027c809bc18895ffdb1fd12dec75ebde93ee757
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2020-03-25 06:43:12 -0500

    creando componente formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
-
+import React from "react";
+import Formulario from "./components/Formulario";
 
 function App() {
   return (
-    <div>
+    <>
       <h1>Administrador de Pacientes</h1>
-    </div>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column"></div>
+        </div>
+      </div>
+    </>
   );
 }
 
