7c9fa4599ac8969a715c4025dd052cd2871a15a7
mahaliroblesarbieto <mahaliroblesarbieto@gmail.com>
2020-03-28 22:43:44 -0500

    mensaje condicional  y funcion de eliminar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Formulario from "./components/Formulario";
+import Cita from "./components/Cita";
 import { useState } from "react";
 
 function App() {
@@ -8,6 +9,15 @@ function App() {
   const obtenerCitas = cita => {
     guardarCitas([...citas, cita]);
   };
+
+  const eliminarCita = (id) => {
+    const newCitas = citas.filter(e => e.id !== id);
+    guardarCitas(newCitas)
+
+  }
+
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
+
   return (
     <>
       <h1>Administrador de Pacientes</h1>
@@ -16,7 +26,16 @@ function App() {
           <div className="one-half column">
             <Formulario obtenerCitas={obtenerCitas} />
           </div>
-          <div className="one-half column"></div>
+          <div className="one-half column">
+          <h2>{titulo}</h2>
+          {citas.map(cita => (
+            <Cita
+            key={cita.id}
+            cita={cita}
+            eliminarCita={eliminarCita}
+            />
+          ))}
+          </div>
         </div>
       </div>
     </>
